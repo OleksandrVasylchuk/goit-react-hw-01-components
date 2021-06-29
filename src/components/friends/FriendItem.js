1eb5c1039a1124a7762b12158be5d2aa91728db6
OleksandrVasylchuk <73894374+OleksandrVasylchuk@users.noreply.github.com>
2021-06-29 19:20:19 +0300

    виправлення помилок

--- src/components/friends/FriendItem.js
+++ src/components/friends/FriendItem.js
@@ -4,7 +4,7 @@ import s from "./Friendlist.module.css";
 const FrendItem = ({ avatar, name, isOnline }) => {
   return (
     <li className={s.item}>
-      <span className={(s.status, isOnline ? s.online : s.offline)}></span>
+      <span className={(s.status, isOnline ? s.online : s.offline)} />
       <img className={s.avatar} src={avatar} alt={name} width="48" />
       <p className={s.name}>{name}</p>
     </li>
